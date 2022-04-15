873d3d1faf3fd5974dbde751b6e9112dfa8aae57
Zohid Valiev <zohid.valiev@gmail.com>
2022-04-15 16:58:20 +0500

    Добавлен метод wrapInTransaction

--- src/Doctrine/Manager.php
+++ src/Doctrine/Manager.php
@@ -9,9 +9,20 @@ class Manager
     public function __construct(private EntityManagerInterface $_em)
     {}
 
+    /**
+     * @deprecated Do not use this method. Since doctrine version 2.10 this method is deprecated.
+     */
     public function transactional($callback)
     {
         return $this->_em->transactional($callback);
+    }
+
+    /**
+     * @return mixed
+     */
+    public function wrapInTransaction(callable $func)
+    {
+        return $this->_em->wrapInTransaction($func);
     }
 
     public function persist($entity)
